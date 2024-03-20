1cbabd0f193a53fe8a546f4c53929b2d5c7f64cc
mina-atallah <minaatallah18@gmail.com>
2024-03-20 23:51:47 +0200

    Run Another Effect - created new effect to playSound whenever the duration state variable changes

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -9,6 +9,17 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
+  /*
+  const playSound = useCallback(
+    function () {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    },
+    [allowSound]
+  );
+*/
+
   // to synchronize duration value with all state variables that duration depends on it
   useEffect(
     function () {
@@ -17,19 +28,27 @@ function Calculator({ workouts, allowSound }) {
     [number, sets, speed, durationBreak]
   );
 
+  // run this effect whenever duration changes
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    [duration, allowSound]
+  );
+
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
-
   function handleIncrement() {
     setDuration((duration) => Math.floor(duration) + 1);
   }
+
   function handleDecrement() {
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
   }
